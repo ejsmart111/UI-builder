0aa3aee5873072c089e51ec72fec5de3b4bb3ed9
ejsmart111 <egbeejay@gmail.com>
2022-11-15 13:58:52 +0100

    Deploying and getting single Js file

--- src/main.js
+++ src/main.js
@@ -4,17 +4,16 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFont, faSquare, faCircle, faStar, faPlay, faSquareMinus } from '@fortawesome/free-solid-svg-icons'
 import './style.css'
-import vueCustomElement from 'vue-custom-element'
+import store from './store'
 // import DropZone from './components/DropZone.vue'
 import 'document-register-element/build/document-register-element'
 
 Vue.config.productionTip = false
-Vue.use(vueCustomElement);
 library.add(faFont, faSquare, faCircle, faStar, faPlay, faSquareMinus)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.customElement('vue-widget', App)
 
-// new Vue({
-//     store,
-//     render: h => h(App)
-// }).$mount('#app')
+
+new Vue({
+    store,
+    render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-const webpack = require('webpack')
-    // module.exports = defineConfig({
-    //   transpileDependencies: true
-    // })
-module.exports = {
-    chainWebpack: (config) => {
-        config.optimization.delete('splitChunks');
-        config
-            .plugin('limitSplitChunks')
-            .use(webpack.optimize.LimitChunkCountPlugin, [{ maxChunks: 1 }]);
-    }
-};
+module.exports = defineConfig({
+    transpileDependencies: true,
+    css: {
+        extract: false,
+    },
+    configureWebpack: {
+        optimization: {
+            splitChunks: false,
+        },
+    },
+})
